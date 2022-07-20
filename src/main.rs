1725bb9363cdba953b5622981eef61372b32e730
John Westhoff <johnjwesthoff@gmail.com>
2022-07-19 23:03:59 -0700

    Working version

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
-use std::io::{self, BufRead};
-use tempfile::tempfile;
+use std::{io::{self, BufRead}, error::Error, process::Command};
+use tempfile::NamedTempFile;
 use std::io::Write;
 
-fn launch(interpreter: String, program: Vec<String>, data: Vec<String>) {
-
+fn fill_file(file: &mut NamedTempFile, data: Vec<String>) -> Result<(), Box<dyn Error>> {
+    for line in data {
+        file.write_all(line.as_bytes())?;
+        file.write(b"\n")?;
+    }
+    Ok(())
 }
 
-fn main() {
+fn launch(interpreter: String, program: Vec<String>, data: Vec<String>) -> Result<(), Box<dyn Error>> {
+    let mut data_file = NamedTempFile::new()?;
+    fill_file(&mut data_file, data)?;
+        let mut program_file = NamedTempFile::new()?;
+    fill_file(&mut program_file, program)?;
+
+    let proc = Command::new(interpreter)
+        .stdin(data_file.reopen()?)
+        .arg(program_file.path().as_os_str())
+        .output()
+        .expect("failed command");
+
+    print!("{}", String::from_utf8(proc.stdout)?);
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     let stdin = io::stdin();
     let mut iterator = stdin.lock().lines();
     let interpreter = iterator.next();
@@ -26,8 +46,9 @@ fn main() {
             }
         }
     }
-    println!("{:?}\n--\n{:?}\n--\n{:?}\n", interpreter, program, data); 
+    //println!("{:?}\n--\n{:?}\n--\n{:?}\n", interpreter, program, data); 
     if let Some(Ok(x)) = interpreter {
-        launch(x, program, data);
+        launch(x, program, data)?;
     }
+    Ok(())
 }
